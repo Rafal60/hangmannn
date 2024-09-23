404694c69b194824a323e837e4e01a3ed22b8d68
Coatleven <rafal.coat@gmail.com>
2024-09-23 11:55:51 +0200

    mot random

--- main.go
+++ main.go
@@ -28,5 +28,26 @@ func main() {
 	for i := 0; i < len(mot); i++ {
 		motcacher = append(motcacher, "_")
 	}
+	nbrand := (len(mot) / 2) - 1
+	for nbrand != 0 {
+		n := rand.IntN(len(mot) - 1)
+		if motcacher[n] == "_" {
+			motcacher[n] = ToUpper(string(mot[n]))
+			nbrand--
+		}
+	}
 	fmt.Println(mot, motcacher)
+
+}
+
+func ToUpper(s string) string {
+	h := []rune(s)
+	result := ""
+	for i := 0; i < len(h); i++ {
+		if (h[i] >= 'a') && (h[i] <= 'z') {
+			h[i] = h[i] - 32
+		}
+		result += string(h[i])
+	}
+	return result
 }
